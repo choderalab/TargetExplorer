e17a244b7989e5a2682ef82be8e00c3206c96e66
Daniel L. Parton <Danny.Parton@gmail.com>
2014-02-17 10:43:47 -0800

    Update.

---
+++ python-scripts/gen-Harvard-plasmids-vs-UniProt.py
@@ -0,0 +1,14 @@
+import os, shutil
+
+main_branch_path = '..'
+html_page_path_main_branch = os.path.join(main_branch_path, 'external-data', 'Harvard-DFHCC-HIP_human_kinase_collection-pJP1520', 'aln-against-UniProt-seq.html')
+
+shutil.copy(html_page_path_main_branch, 'Harvard-plasmids-vs-UniProt.html')
+
+with open('Harvard-plasmids-vs-UniProt.html', 'r') as aln_file:
+    aln_text = aln_file.read()
+
+with open('Harvard-plasmids-vs-UniProt.html', 'w') as aln_file:
+    aln_file.write(aln_text.replace('<link type="text/css" href="seqlib.cs" rel="stylesheet"/>', '<link type="text/css" href="stylesheets/seqlib.css" rel="stylesheet"/>'))
+
+
