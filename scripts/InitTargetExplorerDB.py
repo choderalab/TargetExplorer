a7aebad8251480d0c0bee712e371ab41aa6cae62
Daniel L. Parton <Danny.Parton@gmail.com>
2014-04-10 01:22:17 -0400

    Added MySQL database initialization script, and initial version of SCHEMA

--- scripts/InitTargetExplorerDB.py
+++ scripts/InitTargetExplorerDB.py
@@ -1,21 +1,33 @@
 #!/usr/bin/env python
 #
-# Initialize project directory by creating necessary subdirectories and a project metadata file.
+# Initialize a MySQL database with the TargetExplorer schema
 #
-# Daniel L. Parton <daniel.parton@choderalab.org> - 11 Mar 2014
+# Daniel L. Parton <daniel.parton@choderalab.org> - 9 Apr 2014
 
-import os, argparse
+import os, argparse, getpass
 import TargetExplorer
+import MySQLdb as mdb
 
 # ========
 # Parse command-line arguments
 # ========
 
-argparser = argparse.ArgumentParser(description='Initialize a TargetExplorer database project by creating the necessary subdirectories.')
-argparser.add_argument('--project_dir', type=str, default='.', help='(Default: ".") Optionally provide a directory path in which to initialize the database project.')
+argparser = argparse.ArgumentParser(description='Initialize a MySQL database with the TargetExplorer schema.')
+argparser.add_argument('--db_name', type=str, help='Database name (e.g. kinome)')
+# argparser.add_argument('--host', type=str, help='MySQL hostname')
+argparser.add_argument('--user', type=str, help='MySQL username')
 args = argparser.parse_args()
 
-project_dir = os.path.abspath(args.project_dir)
+user_password = getpass.getpass('Please enter your MySQL password:')
 
-TargetExplorer.core.init_project(project_dir)
+connection = mdb.connect(user=args.user, passwd=user_password)
+with connection:
+    cursor = connection.cursor()
+    cursor.execute("CREATE DATABASE IF NOT EXISTS %s" % args.db_name)
+
+connection = mdb.connect(host='localhost', user=args.user, passwd=user_password, db=args.db_name)
+
+with connection:
+    cursor = connection.cursor(mdb.cursors.DictCursor)
+    cursor.execute("CREATE TABLE IF NOT EXISTS db_entries(_id INT PRIMARY KEY AUTO_INCREMENT)")
 
