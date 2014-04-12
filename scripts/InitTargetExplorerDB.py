26e419dde5ec8b58ee7fe88bd6a0378b09dd9f90
Daniel L. Parton <Danny.Parton@gmail.com>
2014-04-11 22:45:37 -0400

    Added very simple SQLite database initialization script.

--- scripts/InitTargetExplorerDB.py
+++ scripts/InitTargetExplorerDB.py
@@ -1,33 +1,27 @@
 #!/usr/bin/env python
 #
-# Initialize a MySQL database with the TargetExplorer schema
+# Initialize a SQLite database with the TargetExplorer schema
 #
 # Daniel L. Parton <daniel.parton@choderalab.org> - 9 Apr 2014
 
 import os, argparse, getpass
 import TargetExplorer
-import MySQLdb as mdb
+import sqlite3
 
 # ========
 # Parse command-line arguments
 # ========
 
 argparser = argparse.ArgumentParser(description='Initialize a MySQL database with the TargetExplorer schema.')
-argparser.add_argument('--db_name', type=str, help='Database name (e.g. kinome)')
-# argparser.add_argument('--host', type=str, help='MySQL hostname')
-argparser.add_argument('--user', type=str, help='MySQL username')
+argparser.add_argument('--db_name', type=str, help='Database name (e.g. "kinome")')
 args = argparser.parse_args()
 
-user_password = getpass.getpass('Please enter your MySQL password:')
+if not os.path.exists('database'):
+    os.mkdir('database')
 
-connection = mdb.connect(user=args.user, passwd=user_password)
-with connection:
+db_path = os.path.join('database', args.db_name + '.db')
+
+with sqlite3.connect(db_path) as connection:
     cursor = connection.cursor()
-    cursor.execute("CREATE DATABASE IF NOT EXISTS %s" % args.db_name)
-
-connection = mdb.connect(host='localhost', user=args.user, passwd=user_password, db=args.db_name)
-
-with connection:
-    cursor = connection.cursor(mdb.cursors.DictCursor)
-    cursor.execute("CREATE TABLE IF NOT EXISTS db_entries(_id INT PRIMARY KEY AUTO_INCREMENT)")
+    connection.commit()
 
