63ac7976d9f212efb35ea5f1e5fec74c2655dce6
Danny Parton <danny.parton@gmail.com>
2014-05-05 22:23:56 +0000

    flaskapp now serves json data, not html

--- flaskapp/views.py
+++ flaskapp/views.py
@@ -1,20 +1,34 @@
-from flask import render_template, abort
+from flask import abort, jsonify, request, make_response
 import config
 import re
 from flaskapp import app, db, models
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html', title=config.DB_NAME)
+# ======
+# error handlers
+# ======
 
-@app.route('/<string:ac>')
+@app.errorhandler(AssertionError)
+def assertion_error(error):
+    print error.message
+    return make_response(jsonify( { 'error': error.message } ), 404)
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify( { 'error': 'Not found' } ), 404)
+
+# ======
+# Get data for individual target
+# ======
+
+@app.route('/<string:ac>', methods = ['GET'])
 def get_dbentry(ac):
     # check ac is in proper UniProt format
-    # TODO UniProt AC format will be extended some time after June 11 2014!
-    if not (re.match('[A-N,R-Z][0-9][A-Z][A-Z,0-9][A-Z,0-9][0-9]', ac) or re.match('[O,P,Q][0-9][A-Z,0-9][A-Z,0-9][A-Z,0-9][0-9]', ac)):
-        abort(404)
+    # XXX TODO UniProt AC format will be extended some time after June 11 2014!
+    try: assert (re.match('[A-N,R-Z][0-9][A-Z][A-Z,0-9][A-Z,0-9][0-9]', ac) or re.match('[O,P,Q][0-9][A-Z,0-9][A-Z,0-9][A-Z,0-9][0-9]', ac))
+    except AssertionError as e: e.message = 'Incorrect UniProt AC format'; raise e
 
     uniprot = db.session.query(models.UniProt).filter_by(ac=ac).first()
+    try: assert uniprot != None
+    except AssertionError as e: e.message = 'Database entry not found'; raise e
 
-    return render_template('dbentry.html', ac=ac, entry_name=uniprot.entry_name, family=uniprot.family)
+    return jsonify( {'ac': uniprot.ac, 'entry_name': uniprot.entry_name, 'family': uniprot.family} )
