b6905ec15bc6c38631aab5527008b1df3ee42ee5
Daniel Parton <danny.parton@gmail.com>
2014-06-18 18:09:23 -0400

    Fixed template-wsgi.pi

--- resources/template-wsgi.py
+++ resources/template-wsgi.py
@@ -5,7 +5,7 @@ sys.path.insert(0, this_dir)
 from config import targetexplorer_install_dir
 sys.path.insert(0, targetexplorer_install_dir)
 
-import app_master
+from app_master import app
 
 if __name__ == '__main__':
     app_master.app.debug = True
