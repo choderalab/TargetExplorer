2c08aa9f422675c345a3d1eae54877b540dc3220
Daniel L. Parton <Danny.Parton@gmail.com>
2014-08-06 00:40:08 -0400

    Removed some old files

--- scripts/DoraRunFlask.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import os, sys
-this_dir = os.path.dirname(os.path.realpath(__file__))
-target_explorer_basedir = os.path.realpath(os.path.join(this_dir, '..'))
-sys.path.append(target_explorer_basedir)
-
-import app_master
-app_master.app.run(debug=True)
