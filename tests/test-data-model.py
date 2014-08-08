4ec9b9ecf95342183e158b622b0bc2591836756a
Daniel L. Parton <Danny.Parton@gmail.com>
2014-08-08 18:05:30 -0400

    Added test for asserting that data tables all have crawl_number column

---
+++ tests/test-data-model.py
@@ -0,0 +1,7 @@
+def test_all_table_classes_have_crawl_number_attrib():
+    from flaskapp import models
+    for table_class_name in models.table_class_names:
+        if table_class_name == 'CrawlData':
+            continue
+        table_class = getattr(models, table_class_name)
+        assert hasattr(table_class, 'crawl_number')
