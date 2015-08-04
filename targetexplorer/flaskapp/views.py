307186d35c1830e3d1a60d2661d71a1eaf2e567b
Daniel L. Parton <Danny.Parton@gmail.com>
2015-08-04 17:03:25 -0400

    Minor refactoring of web API

--- targetexplorer/flaskapp/views.py
+++ targetexplorer/flaskapp/views.py
@@ -1,72 +1,33 @@
-from flask import abort, jsonify, request, make_response, current_app
 import re
-from datetime import timedelta
-from functools import update_wrapper
+from flask import abort, jsonify, request, make_response
 from targetexplorer.flaskapp import app, db, models
 from targetexplorer.core import read_project_config
+from targetexplorer.flaskapp.webapi_utils import crossdomain
 
 project_config = read_project_config()
 dbapi_name = project_config['dbapi_name']
 
 
 # ======
-# HTTP access control decorator - for cross-origin resource sharing (CORS)
+# Get database metadata
 # ======
 
-def crossdomain(origin=None, methods=None, headers=None,
-                max_age=21600, attach_to_all=True,
-                automatic_options=True):
-    if methods is not None:
-        methods = ', '.join(sorted(x.upper() for x in methods))
-    if headers is not None and not isinstance(headers, basestring):
-        headers = ', '.join(x.upper() for x in headers)
-    if not isinstance(origin, basestring):
-        origin = ', '.join(origin)
-    if isinstance(max_age, timedelta):
-        max_age = max_age.total_seconds()
+# Examples:
+# http://.../[DB_NAME]DBAPI/get_metadata
 
-    def get_methods():
-        if methods is not None:
-            return methods
+@app.route('/%s/get_metadata' % dbapi_name, methods=['GET'])
+@crossdomain(origin='*', headers=["Origin", "X-Requested-With", "Content-Type", "Accept"])
+def get_metadata():
+    project_config = read_project_config()
+    results_obj = {
+        'uniprot_query': project_config['uniprot_query'],
+        'uniprot_domain_regex': project_config['uniprot_domain_regex'],
+    }
 
-        options_resp = current_app.make_default_options_response()
-        return options_resp.headers['allow']
+    # = Return data in JSON format =
+    response = make_response(jsonify(results_obj))
+    return response
 
-    def decorator(f):
-        def wrapped_function(*args, **kwargs):
-            if automatic_options and request.method == 'OPTIONS':
-                resp = current_app.make_default_options_response()
-            else:
-                resp = make_response(f(*args, **kwargs))
-            if not attach_to_all and request.method != 'OPTIONS':
-                return resp
-
-            h = resp.headers
-
-            h['Access-Control-Allow-Origin'] = origin
-            h['Access-Control-Allow-Methods'] = get_methods()
-            h['Access-Control-Max-Age'] = str(max_age)
-            if headers is not None:
-                h['Access-Control-Allow-Headers'] = headers
-            return resp
-
-        f.provide_automatic_options = False
-        f.required_methods = ['OPTIONS']
-        return update_wrapper(wrapped_function, f)
-    return decorator
-
-# ======
-# error handlers
-# ======
-
-@app.errorhandler(AssertionError)
-def assertion_error(error):
-    print error.message
-    return make_response(jsonify( { 'error': error.message } ), 404)
-
-@app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify( { 'error': 'Not found' } ), 404)
 
 # ======
 # Get list of all db entries
@@ -129,24 +90,21 @@ def get_db_entry():
                 ):
             abort(404)
 
-    # = Search the UniProt table using the query AC =
+    # = Search the UniProt table =
+    # e.g. uniprot_row = models.UniProtEntry.query.filter(models.UniProtEntry.ac == 'P00519', models.UniProtEntry.crawl_number == 0).first()
     model_request_field = getattr(models.UniProtEntry, request_field)
     uniprot_row = models.UniProtEntry.query.filter(
         model_request_field == request_value, models.UniProtEntry.crawl_number == safe_crawl_number
     ).first()
 
-    try:
-        assert uniprot_row != None
-    except AssertionError as e:
-        e.message = 'Database entry not found'
-        raise e
+    if uniprot_row is None:
+        raise UniProtEntryNotFoundError('{}={}'.format(request_field, request_value))
 
     # = Retrieve the corresponding DBEntry row =
     db_entry = uniprot_row.db_entry
-    # db_entry = db.session.query(models.DBEntry).filter_by(id=uniprot_row.db_entry_id, crawl_number=safe_crawl_number).first()
 
     # = Construct the data structure for holding the results, to be returned as JSON =
-    target_obj = {
+    db_entry_obj = {
         'uniprot': {
             'ac': uniprot_row.ac,
             'entry_name': uniprot_row.entry_name,
@@ -163,24 +121,24 @@ def get_db_entry():
     # = Add info from other tables =
     # PDB
     for pdb in db_entry.pdbs:
-        target_obj['pdb'].append({'pdb_id': pdb.pdb_id})
+        db_entry_obj['pdb'].append({'pdb_id': pdb.pdb_id})
 
     # HGNC
     for entry in db_entry.hgnc_entries:
-        target_obj['hgnc'].append(
+        db_entry_obj['hgnc'].append(
             {'gene_id': entry.gene_id, 'approved_symbol': entry.approved_symbol}
         )
 
     # Ensembl Gene
     for entry in db_entry.ensembl_genes:
-        target_obj['ensembl_gene'].append({'gene_id': entry.gene_id})
+        db_entry_obj['ensembl_gene'].append({'gene_id': entry.gene_id})
 
     # NCBI Gene
     for entry in db_entry.ncbi_gene_entries:
-        target_obj['ncbi_gene'].append({'gene_id': entry.gene_id})
+        db_entry_obj['ncbi_gene'].append({'gene_id': entry.gene_id})
 
     # = Return data in JSON format =
-    response = make_response(jsonify(target_obj))
+    response = make_response(jsonify(db_entry_obj))
     return response
 
 
@@ -189,6 +147,7 @@ def get_db_entry():
 # ======
 
 # Examples:
+# http://.../[DB_NAME]DBAPI/search?query=family="TK" AND npubs>0&return="domain_seqs"
 # http://.../[DB_NAME]DBAPI/search?query=family="TK" AND db_target_rank<300&return="domain_seqs"
 
 # Example SQLAlchemy filter syntax:
@@ -233,7 +192,6 @@ def query_db():
 
     # Use the query string to filter DBEntry rows
     results = query.filter(sql_query_string)
-    print sql_query_string
 
     # = Construct the data structure for holding the results, to be returned as JSON =
     targets_obj = {'results': []}
@@ -276,22 +234,26 @@ def query_db():
     response = make_response(jsonify(targets_obj))
     return response
 
+
 # ======
-# Get database metadata
+# error handlers
 # ======
 
-# Examples:
-# http://.../[DB_NAME]DBAPI/get_metadata
 
-@app.route('/%s/get_metadata' % dbapi_name, methods=['GET'])
-@crossdomain(origin='*', headers=["Origin", "X-Requested-With", "Content-Type", "Accept"])
-def get_metadata():
-    project_config = read_project_config()
-    results_obj = {
-        'uniprot_query': project_config['uniprot_query'],
-        'uniprot_domain_regex': project_config['uniprot_domain_regex'],
-    }
+class UniProtEntryNotFoundError(Exception):
+    pass
 
-    # = Return data in JSON format =
-    response = make_response(jsonify(results_obj))
-    return response
+
+@app.errorhandler(UniProtEntryNotFoundError)
+def return_uniprot_entry_not_found_error(error):
+    return make_response(
+        jsonify({
+            'error': 'UniProt entry not found',
+            'message': error.message,
+        })
+    )
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
--- targetexplorer/resources/template-wsgi.py
+++ targetexplorer/resources/template-wsgi.py
@@ -1,13 +1,13 @@
-import os, sys
+# import os, sys
 
-this_dir = os.path.dirname(os.path.realpath(__file__))
-sys.path.insert(0, this_dir)
-from project_config import targetexplorer_install_dir
-sys.path.insert(0, targetexplorer_install_dir)
+# this_dir = os.path.dirname(os.path.realpath(__file__))
+# sys.path.insert(0, this_dir)
+# from project_config import targetexplorer_install_dir
+# sys.path.insert(0, targetexplorer_install_dir)
 
 from targetexplorer.flaskapp import app
 
 if __name__ == '__main__':
-    from targetexplorer import flaskapp
-    flaskapp.app.debug = True
-    flaskapp.app.run()
+    from targetexplorer.flaskapp import app, views
+    app.debug = True
+    app.run()
