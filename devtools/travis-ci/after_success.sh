e3a1bf88814aabfa027e1e241d58ead4c6e7bd90
Daniel L. Parton <Danny.Parton@gmail.com>
2015-08-05 17:03:20 -0400

    Trying to get conda-server upload to work

--- devtools/travis-ci/after_success.sh
+++ devtools/travis-ci/after_success.sh
@@ -1,4 +1,7 @@
-echo $TRAVIS_PULL_REQUEST $TRAVIS_BRANCH
+#!/bin/bash
+
+echo $TRAVIS_PULL_REQUEST
+echo $TRAVIS_BRANCH
 
 if [[ "$TRAVIS_PULL_REQUEST" == "true" ]]; then
     echo "This is a pull request. No deployment will be done."; exit 0
@@ -9,15 +12,6 @@ if [[ "$TRAVIS_BRANCH" != "master" ]]; then
     echo "No deployment on BRANCH='$TRAVIS_BRANCH'"; exit 0
 fi
 
-
-if [[ "2.7 3.3 3.4" =~ "$python" ]]; then
-conda install --yes --quiet conda-server
-echo "Attempting to upload to conda-server:"
-echo "conda-server -t [BINSTAR_TOKEN]  upload --force -u choderalab $HOME/miniconda/conda-bld/linux-64/targetexplorer-*"
-conda-server -h
-conda-server -t ${BINSTAR_TOKEN}  upload --force -u choderalab $HOME/miniconda/conda-bld/linux-64/targetexplorer-*.tar.bz2
-fi
-
-if [[ "$python" != "2.7" ]]; then
-    echo "No deploy on PYTHON_VERSION=${python}"; exit 0
-fi
+# Deploy to binstar.
+conda install --yes anaconda-client jinja2
+binstar -t $BINSTAR_TOKEN upload --force -u choderalab -p targetexplorer-dev $HOME/miniconda/conda-bld/*/targetexplorer-dev-*.tar.bz2
