acb47e67ae8aa1b4548950bfb927843d47cce111
Daniel L. Parton <Danny.Parton@gmail.com>
2015-08-18 12:08:13 -0400

    Trying to get Travis CI and binstar upload working

--- devtools/travis-ci/after_success.sh
+++ devtools/travis-ci/after_success.sh
@@ -3,18 +3,18 @@ echo $TRAVIS_PULL_REQUEST $TRAVIS_BRANCH
 
 deploy="true"
 
-if [[ "$TRAVIS_PULL_REQUEST" == "true" ]]; then
+if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
     echo "This is a pull request. No deployment will be done."
     deploy="false"
 fi
+
 if [[ "$TRAVIS_BRANCH" != "master" ]]; then
     echo "No deployment on BRANCH='$TRAVIS_BRANCH'"
     deploy="false"
 fi
 
+echo $deploy
+
 if [[ "$deploy" == "true" ]]; then
     binstar -t $BINSTAR_TOKEN upload --force -u choderalab -p targetexplorer-dev $HOME/miniconda/conda-bld/linux-64/targetexplorer-dev-*
 fi
-
-# Run tests with external dependencies (Travis build should not depend upon this since, for example, external sites could be temporarily down)
-nosetests targetexplorer -v --exe -A 'network and not slow'
